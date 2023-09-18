fe235f879a9057205720e24c32cb3f32b8537fed
Daniel McGrath <djmcgrath95@gmail.com>
2023-09-18 09:47:24 -0600

    Revised Completed Fetch Lab

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   return fetch("https://anapioficeandfire.com/api/books")
   .then(response => response.json())
-  .then(books => {
-    console.log(books)
-    renderBooks(books)
-  })
-}
+  .then(json => renderBooks(json))
+  }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
